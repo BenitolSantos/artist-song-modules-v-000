2a28d5243ee2e7cf182ac3c26ff75f809ac49fcf
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-05-21 18:09:08 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -19,6 +19,8 @@ class Artist
     @@artists
   end
 
+  extend Memorable
+=begin
   def self.reset_all
     self.all.clear
   end
@@ -26,7 +28,7 @@ class Artist
   def self.count
     @@artists.count
   end
-
+=end
   def add_song(song)
     @songs << song
     song.artist = self
